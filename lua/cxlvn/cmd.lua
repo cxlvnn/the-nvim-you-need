732f5b28c2106c68668eb4fa1010105d4a6b2847
cxlvnn <behruzturabekov19@gmail.com>
2026-03-24 23:46:10 +0500

    removed that annoying random cursor jumps

--- lua/cxlvn/cmd.lua
+++ lua/cxlvn/cmd.lua
@@ -26,3 +26,18 @@ vim.api.nvim_create_user_command("ReloadConfig", function()
   dofile(vim.env.MYVIMRC)
   print("✅ Config reloaded!")
 end, {})
+
+function leave_snippet()
+    if
+        ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+        require('luasnip').unlink_current()
+    end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+    autocmd ModeChanged * lua leave_snippet()
+]])
--- lua/cxlvn/plugins/init.lua
+++ lua/cxlvn/plugins/init.lua
@@ -43,58 +43,58 @@ return {
   -- lazygit
   { "kdheepak/lazygit.nvim", },
 
-  -- { -- Autocompletion
-  --   'saghen/blink.cmp',
-  --   event = 'VimEnter',
-  --   version = '1.*',
-  --   dependencies = {
-  --     {
-  --       'L3MON4D3/LuaSnip',
-  --       version = '2.*',
-  --       build = (function()
-  --         return 'make install_jsregexp'
-  --       end)(),
-  --       dependencies = {
-  --         {
-  --           'rafamadriz/friendly-snippets',
-  --           config = function()
-  --             require('luasnip.loaders.from_vscode').lazy_load()
-  --           end,
-  --         },
-  --       },
-  --       opts = {},
-  --     },
-  --     'folke/lazydev.nvim',
-  --   },
-  --   opts = {
-  --     keymap = {
-  --       preset = 'super-tab',
-  --     },
-  --
-  --     appearance = {
-  --       nerd_font_variant = 'mono',
-  --     },
-  --
-  --     completion = {
-  --       documentation = { auto_show = true, auto_show_delay_ms = 500 },
-  --     },
-  --
-  --     sources = {
-  --       default = { 'lsp', 'path', 'snippets', 'lazydev' },
-  --       per_filetype = {
-  --         php = { 'lsp', 'path', 'snippets', 'lazydev' },
-  --       },
-  --       providers = {
-  --         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-  --       },
-  --     },
-  --
-  --     snippets = { preset = 'luasnip' },
-  --     fuzzy = { implementation = 'lua' },
-  --
-  --     signature = { enabled = true },
-  --   },
-  -- },
+  { -- Autocompletion
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      'folke/lazydev.nvim',
+    },
+    opts = {
+      keymap = {
+        preset = 'super-tab',
+      },
+
+      appearance = {
+        nerd_font_variant = 'mono',
+      },
+
+      completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        per_filetype = {
+          php = { 'lsp', 'path', 'snippets', 'lazydev' },
+        },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        },
+      },
+
+      snippets = { preset = 'luasnip' },
+      fuzzy = { implementation = 'lua' },
+
+      signature = { enabled = true },
+    },
+  },
 
   -- mason
   {
